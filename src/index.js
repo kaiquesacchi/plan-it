3640664bed5e94884b565b67acf99746e30ba9ab
Kaique Sacchi <sacchikaique@gmail.com>
2020-01-02 01:47:32 -0200

    Added React Router.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import Project from './pages/Project';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/project" component={Project} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/Project/Project.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Project() {
+  return <h1>Project Page</h1>;
+}
+export default Project;
