c4680d54518f2f3c32fde316447b1986ecdde10d
Kaique Sacchi <sacchikaique@gmail.com>
2020-01-06 01:35:27 -0200

    Added Header component.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import './Header.sass';
+
+function Header({ title, children }) {
+  const bannerHeight = 220;
+
+  const [bannerPadding, setBannerPadding] = useState(0);
+  const [bannerOpacity, setBannerOpacity] = useState(1);
+  const [headerOpacity, setHeaderOpacity] = useState(0);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      const pageYOffset = window.pageYOffset;
+      if (pageYOffset <= bannerHeight) {
+        setBannerPadding(pageYOffset);
+        setHeaderOpacity(1 - (bannerHeight - pageYOffset) / (pageYOffset + 0.1));
+        setBannerOpacity((bannerHeight - pageYOffset) / (pageYOffset + 0.1));
+      } else {
+        if (headerOpacity < 1) setHeaderOpacity(1);
+      }
+    };
+
+    return () => (window.onscroll = null);
+  }, []);
+
+  return (
+    <section id="component-Header">
+      <div className="banner">
+        <h1 style={{ opacity: bannerOpacity, marginTop: bannerPadding }}>{title}</h1>
+      </div>
+      <header id="sticky-header">
+        <h1 style={{ opacity: headerOpacity }}>{title}</h1>
+        <div className="buttons">{children}</div>
+      </header>
+    </section>
+  );
+}
+export default Header;
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import './Projects.sass';
 
+import { FaPlus } from 'react-icons/fa';
+import { AiOutlineDelete } from 'react-icons/ai';
+
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
 function Projects() {
-  return <h1>Projects Page</h1>;
+  return (
+    <div className="page-Projects">
+      <Header title="My Projects">
+        <FaPlus />
+        <AiOutlineDelete />
+      </Header>
+      <div style={{ height: '200vh' }}>Content</div>
+      <Footer />
+    </div>
+  );
 }
 export default Projects;
