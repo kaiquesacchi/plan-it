85db18334010203ec77ab61b9cd0a89255ea05a0
Kaique Sacchi <sacchikaique@gmail.com>
2020-01-06 23:18:35 -0200

    Added (Settings page) Basic structure.

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -4,10 +4,22 @@ import './Settings.sass';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
+import Toggle from '../../components/inputs/Toggle';
+
 function Settings() {
   return (
-    <div className="page-Settings">
+    <div id="page-Settings">
       <Header title="Settings" />
+      <section id="content">
+        <ul className="group">
+          <li>
+            Config name
+            <Toggle />
+          </li>
+          <li>Config name</li>
+          <li>Config name</li>
+        </ul>
+      </section>
       <Footer active="Settings" />
     </div>
   );
