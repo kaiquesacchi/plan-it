4b9da94946bbbac7888d5ad3ef9ef566e6390b9b
Kaique Sacchi <sacchikaique@gmail.com>
2020-01-11 16:00:13 -0200

    Refactored (Toggle) Using styled-components.

--- src/components/inputs/Toggle/Toggle.js
+++ src/components/inputs/Toggle/Toggle.js
@@ -1,5 +1,67 @@
 import React, { useState } from 'react';
-import './Toggle.sass';
+
+import styled, { keyframes } from 'styled-components';
+
+const bodyWidth = '36px';
+const bodyHeight = '16px';
+const selectorSize = '20px';
+
+const toggleOn = keyframes`
+    from {
+      left: -5px;
+    }
+    to {
+      left: ${bodyWidth} - ${selectorSize} + 5px;
+    }
+`;
+
+const toggleOff = keyframes`
+    from {
+      left: ${bodyWidth} - ${selectorSize} + 5px;
+    }
+    to {
+      left: -5px;
+    }
+`;
+
+const Switch = styled.div`
+  width: ${bodyWidth};
+  height: ${bodyHeight};
+  border-radius: ${bodyHeight};
+  border-width: 1px;
+  border-style: solid;
+  display: flex;
+  align-items: center;
+  &.on {
+    background-color: #008bfd;
+  }
+
+  input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+`;
+
+const Slider = styled.div`
+  width: ${selectorSize};
+  height: ${selectorSize};
+  border-radius: ${selectorSize};
+  border-width: 1.5px;
+  border-style: solid;
+  background-color: white;
+  position: relative;
+
+  &.off {
+    left: -5px;
+    animation: ${toggleOff} 0.3s ease;
+  }
+  &.on {
+    left: calc(${bodyWidth} - ${selectorSize} + 5px);
+    animation: ${toggleOn} 0.3s ease;
+    border-color: #008bfd;
+  }
+`;
 
 function Toggle({ initial = false, callback = () => {} }) {
   const [value, setValue] = useState(initial ? 'on' : 'off');
@@ -12,11 +74,9 @@ function Toggle({ initial = false, callback = () => {} }) {
   };
 
   return (
-    <div id="component-Toggle" onClick={onClick}>
-      <div className={'body ' + value}>
-        <div className={'selector ' + value} />
-      </div>
-    </div>
+    <Switch onClick={onClick} className={value}>
+      <Slider className={value} />
+    </Switch>
   );
 }
 export default Toggle;
