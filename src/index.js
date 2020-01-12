b53ffad10c6215493ff78316e338322373412d58
Kaique Sacchi <sacchikaique@gmail.com>
2020-01-12 01:54:06 -0200

    Added cordova.

--- src/index.js
+++ src/index.js
@@ -56,9 +56,15 @@ function App() {
     </ThemeContext.Provider>
   );
 }
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const startApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+};
+
+if (window.cordova) {
+  document.addEventListener('deviceready', startApp, false);
+} else startApp();
