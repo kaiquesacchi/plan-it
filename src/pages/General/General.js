d027b444fb562f2e3d3fbd039e29a9fd9219d944
Kaique Sacchi <sacchikaique@gmail.com>
2020-01-14 19:15:12 -0200

    Added (General page) Basic structure.

--- src/pages/General/General.js
+++ src/pages/General/General.js
@@ -3,12 +3,28 @@ import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
+import ProjectsService from '../../services/Projects';
+
+import { Page } from './styles';
+
 function General() {
+  const projects = ProjectsService.list();
   return (
-    <div id="page-General" className="outerBackground">
+    <Page>
       <Header title="General" />
+      <section id="content">
+        <ul>
+          <li>
+            <span>Projects</span>
+            <span>{projects.length}</span>
+          </li>
+          <li>Example</li>
+          <li>Example</li>
+          <li>Example</li>
+        </ul>
+      </section>
       <Footer active="General" />
-    </div>
+    </Page>
   );
 }
 export default General;
--- src/pages/General/styles.js
+++ src/pages/General/styles.js
@@ -1,3 +1,32 @@
 import styled from 'styled-components';
 
-export const Page = styled.section``;
+export const Page = styled.section`
+  background-color: ${props => props.theme.outerBackgroundColor};
+  color: ${props => props.theme.color};
+  section#content {
+    box-sizing: border-box;
+    min-height: calc(100vh - 80px - 60px);
+    ul {
+      padding: 0 5%;
+      background-color: ${props => props.theme.innerBackgroundColor};
+      border-radius: 20px;
+      border: 1px solid ${props => props.theme.border};
+      list-style-type: none;
+      margin-bottom: 20px;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+      li {
+        padding: 10px 0;
+        border-bottom: 1px solid ${props => props.theme.border};
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        &:last-child {
+          border: none;
+        }
+      }
+    }
+  }
+`;
